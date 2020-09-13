368486e4afdf0b7efa71d3572a3be8d15bd50145
yangguanghe <1649063797@qq.com>
2020-09-14 00:43:10 +0800

    还原二叉树

--- index.js
+++ index.js
@@ -1,79 +1,48 @@
-// 二维数据结构（图）
+var qian = ['a', 'c', 'f', 'g', 'b', 'd', 'e'];
+var zhong = ['f', 'c', 'g', 'a', 'd', 'b', 'e'];
+var hou = ['f', 'g', 'c', 'd', 'e', 'b', 'a'];
 
-// function Node(value) {
-//     this.value = value;
-//     this.neighbor = []
-// }
-
-// var a = new Node('a');
-// var b = new Node('b');
-// var c = new Node('c');
-// var d = new Node('d');
-// var e = new Node('e');
-// var f = new Node('f');
-
-
-// a.neighbor.push(b);
-// a.neighbor.push(c);
-// a.neighbor.push(f);
-
-// b.neighbor.push(a);
-// b.neighbor.push(d);
-// b.neighbor.push(e);
-
-// c.neighbor.push(a);
-
-// d.neighbor.push(b);
-
-// b.neighbor.push(b);
-
-// 二叉树的前序遍历
 function Node(value) {
     this.value = value
     this.left = null
     this.right = null
 }
-
-var a = new Node('a')
-var b = new Node('b')
-var c = new Node('c')
-var d = new Node('d')
-var e = new Node('e')
-var f = new Node('f')
-var g = new Node('g')
-
-a.left = c
-a.right = b
-c.left = f
-c.right = g
-b.left = d
-b.right = e
-
-function f1(root) {
-    if (root === null) return;
-    console.log(root.value)
-    f1(root.left)
-    f1(root.right)
+// 根据前序中序还原二叉树
+function f1(qian, zhong) {
+    if (qian == null || zhong == null || qian.length == 0 || zhong.length == 0 || qian.length !== zhong.length) return null;
+    var root = new Node(qian[0]);
+    // 找到根节点在中序遍历的位置
+    var index = zhong.indexOf(root.value);
+    var qianLeft = qian.slice(1, index + 1); // 前序遍历的左子树
+    var qianRight = qian.slice(index + 1, qian.length); // 前序遍历的右子树
+    var zhongLeft = zhong.slice(0, index); // 中序遍历的左子树
+    var zhongRight = zhong.slice(index + 1, zhong.length); // 中序遍历的右子树
+    root.left = f1(qianLeft, zhongLeft);
+    root.right = f1(qianRight, zhongRight);
+    return root;
 }
-f1(a)
 
-console.log('*****************')
-// 中序遍历
-function f1(root) {
-    if (root === null) return;
-    f1(root.left)
-    console.log(root.value)
-    f1(root.right)
+// var root = f1(qian, zhong);
+
+// console.log(root.left);
+// console.log(root.right);
+
+// 根据中序后序还原二叉树
+function f2(zhong, hou) {
+    if (zhong == null || hou == null || zhong.length == 0 || hou.length == 0 || zhong.length !== hou.length) return null;
+    var root = new Node(hou[hou.length - 1]);
+    // 找到根节点在中序遍历的位置
+    var index = zhong.indexOf(root.value);
+    var zhongLeft = zhong.slice(0, index); // 中序遍历的左子树
+    var zhongRight = zhong.slice(index + 1, zhong.length); // 中序遍历的右子树
+    var houLeft = hou.slice(0, index); // 后序遍历的左子树
+    var houRight = hou.slice(index, hou.length - 1); // 后序遍历的右子树
+    root.left = f2(zhongLeft, houLeft);
+    root.right = f2(zhongRight, houRight);
+    return root;
 }
-f1(a)
 
+var root1 = f2(zhong, hou);
 
-console.log('*****************')
-// 后序遍历
-function f1(root) {
-    if (root === null) return;
-    f1(root.left)
-    f1(root.right)
-    console.log(root.value)
-}
-f1(a)
+console.log(root1.left);
+console.log(root1.right);
