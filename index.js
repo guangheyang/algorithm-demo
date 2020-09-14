5ddf85438469357abe5819429bbfa2aafa9aa0ce
yangguanghe <1649063797@qq.com>
2020-09-14 22:07:51 +0800

    二叉树的搜索

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-var qian = ['a', 'c', 'f', 'g', 'b', 'd', 'e'];
-var zhong = ['f', 'c', 'g', 'a', 'd', 'b', 'e'];
-var hou = ['f', 'g', 'c', 'd', 'e', 'b', 'a'];
-
 function Node(value) {
     this.value = value
     this.left = null
     this.right = null
 }
-// 根据前序中序还原二叉树
-function f1(qian, zhong) {
-    if (qian == null || zhong == null || qian.length == 0 || zhong.length == 0 || qian.length !== zhong.length) return null;
-    var root = new Node(qian[0]);
-    // 找到根节点在中序遍历的位置
-    var index = zhong.indexOf(root.value);
-    var qianLeft = qian.slice(1, index + 1); // 前序遍历的左子树
-    var qianRight = qian.slice(index + 1, qian.length); // 前序遍历的右子树
-    var zhongLeft = zhong.slice(0, index); // 中序遍历的左子树
-    var zhongRight = zhong.slice(index + 1, zhong.length); // 中序遍历的右子树
-    root.left = f1(qianLeft, zhongLeft);
-    root.right = f1(qianRight, zhongRight);
-    return root;
+
+var a = new Node('a');
+var b = new Node('b');
+var c = new Node('c');
+var d = new Node('d');
+var e = new Node('e');
+var f = new Node('f');
+var g = new Node('g');
+
+a.left = c;
+a.right = b;
+c.left = f;
+c.right = g;
+b.left = d;
+b.right = e;
+
+function deepSearch(root, target) {
+    if (root == null) return false;
+    if (root.value == target) return true;
+    var left = deepSearch(root.left, target);
+    var right = deepSearch(root.right, target);
+    return left || right
 }
 
-// var root = f1(qian, zhong);
+console.log(deepSearch(a, 's'))
 
-// console.log(root.left);
-// console.log(root.right);
 
-// 根据中序后序还原二叉树
-function f2(zhong, hou) {
-    if (zhong == null || hou == null || zhong.length == 0 || hou.length == 0 || zhong.length !== hou.length) return null;
-    var root = new Node(hou[hou.length - 1]);
-    // 找到根节点在中序遍历的位置
-    var index = zhong.indexOf(root.value);
-    var zhongLeft = zhong.slice(0, index); // 中序遍历的左子树
-    var zhongRight = zhong.slice(index + 1, zhong.length); // 中序遍历的右子树
-    var houLeft = hou.slice(0, index); // 后序遍历的左子树
-    var houRight = hou.slice(index, hou.length - 1); // 后序遍历的右子树
-    root.left = f2(zhongLeft, houLeft);
-    root.right = f2(zhongRight, houRight);
-    return root;
+function rangeSearch(rootList, target) {
+    if (rootList == null || rootList.length == 0) return false;
+    var childList = []
+    for (var i = 0; i < rootList.length; i++) {
+        console.log(rootList[i].value)
+        if (rootList[i] != null && rootList[i].value == target) {
+            return true
+        } else {
+            childList.push(rootList[i].left);
+            childList.push(rootList[i].right);
+        }
+    }
+    return rangeSearch(childList, target);
 }
 
-var root1 = f2(zhong, hou);
-
-console.log(root1.left);
-console.log(root1.right);
+console.log(rangeSearch([a], 'f'))
