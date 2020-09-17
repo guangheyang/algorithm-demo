3b201a419365e345f649655f2aa8115e5c18ffe6
yangguanghe <1649063797@qq.com>
2020-09-18 01:03:20 +0800

    平衡二叉树

--- index.js
+++ index.js
@@ -1,63 +1,48 @@
 // 有一万个数，写一个方法进行查找指定的数，返回存在还是不存在，要求：尽可能性能好
-var arr = []
-for (var i = 0; i < 10000; i++) {
-    arr[i] = Math.floor(Math.random() * 10000);
-}
-
-var num = 0;
-// 普通方法
-function search(arr, target) {
-    for (var i = 0; i < arr.length; i++) {
-        num += 1
-        if (arr[i] == target) return true;
-    }
-    return false;
-}
-
-console.log(search(arr, 1000));
-console.log(num);
-
-// 构建二叉搜索树
+// 判断平衡二叉树
 function Node(value) {
     this.value = value;
     this.left = null;
     this.right = null;
 }
 
-function addNode(root, num) {
-    if (root == null) return;
-    if (root.value == num) return;
-    if (root.value < num) {
-        if (root.right == null) root.right = new Node(num);
-        else addNode(root.right, num);
+var a = new Node('a');
+var b = new Node('b');
+var c = new Node('c');
+var d = new Node('d');
+var e = new Node('e');
+var f = new Node('f');
+var g = new Node('g');
+var h = new Node('h');
+var i = new Node('i');
+
+a.left = b;
+a.right = c;
+b.left = d;
+// b.right = e;
+c.left = f;
+c.right = g;
+d.right = h;
+// e.right = i;
+
+// 获取二叉树深度
+function getDeep(root) {
+    if (root == null) return 0;
+    var leftDeep = getDeep(root.left);
+    var rightDeep = getDeep(root.right);
+    return Math.max(leftDeep, rightDeep) + 1;
+}
+
+// 是否为平衡二叉树
+function isBalance(root) {
+    if (root == null) return true;
+    var leftDeep = getDeep(root.left);
+    var rightDeep = getDeep(root.right);
+    if (Math.abs(leftDeep - rightDeep) > 1) {
+        return false;
     } else {
-        if (root.left == null) root.left = new Node(num);
-        else addNode(root.left, num)
+        return isBalance(root.left) && isBalance(root.right);
     }
 }
 
-function buildSearchTree(arr) {
-    if (arr == null || arr.length == 0) return null;
-    var root = new Node(arr[0]);
-    for (var i = 0; i < arr.length; i++) {
-        addNode(root, arr[i]);
-    }
-
-    return root;
-}
-
-var root = buildSearchTree(arr);
-
-// 使用二叉搜索树
-var index = 0
-function searchByTree(root, target) {
-    if (root == null) return false;
-    index += 1
-    if (root.value == target) return true;
-    if (root.value > target) return searchByTree(root.left, target);
-    else return searchByTree(root.right, target);
-}
-
-var result = searchByTree(root, 1000);
-console.log(result)
-console.log(index)
+console.log(isBalance(a));
