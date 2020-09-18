d4b4783df4ebc2176cb7ae095ead5b704f5343c6
yangguanghe <1649063797@qq.com>
2020-09-19 00:08:28 +0800

    二叉树单旋

--- index.js
+++ index.js
@@ -6,24 +6,13 @@ function Node(value) {
     this.right = null;
 }
 
-var a = new Node('a');
-var b = new Node('b');
-var c = new Node('c');
-var d = new Node('d');
-var e = new Node('e');
-var f = new Node('f');
-var g = new Node('g');
-var h = new Node('h');
-var i = new Node('i');
-
-a.left = b;
-a.right = c;
-b.left = d;
-// b.right = e;
-c.left = f;
-c.right = g;
-d.right = h;
-// e.right = i;
+var node2 = new Node('2');
+var node5 = new Node('5');
+var node3 = new Node('3');
+var node6 = new Node('6');
+node2.right = node5;
+node5.left = node3;
+node5.right = node6;
 
 // 获取二叉树深度
 function getDeep(root) {
@@ -45,4 +34,50 @@ function isBalance(root) {
     }
 }
 
-console.log(isBalance(a));
+// 左旋
+function leftRotate(root) {
+    // 找到新根
+    var newRoot = root.right;
+    // 找到变化分支
+    var changeTree = root.right.left;
+    // 当前旋转节点的右孩子为变化分支
+    root.right = changeTree;
+    // 新根的左孩子为旋转节点
+    newRoot.left = root;
+    // 返回新的根节点
+    return newRoot;
+}
+// 右旋
+function rightRotate(root) {
+    // 找到新根
+    var newRoot = root.left;
+    // 找到变化分支
+    var changeTree = root.left.right;
+    // 当前旋转节点的左孩子为变化分支
+    root.left = changeTree;
+    // 新根的右孩子为旋转节点
+    newRoot.right = root;
+    // 返回新的根节点
+    return newRoot;
+}
+
+function change(root) {
+    if (isBalance(root)) return root;
+    if (root.left != null) root.left = change(root.left);
+    if (root.right != null) root.right = change(root.right);
+    var leftDeep = getDeep(root.left);
+    var rightDeep = getDeep(root.right);
+    if (Math.abs(leftDeep - rightDeep) < 2) {
+        return true;
+    } else if (leftDeep > rightDeep) {
+        return rightRotate(root);
+    } else {
+        return leftRotate(root);
+    }
+}
+
+console.log(isBalance(node2));
+
+var newRoot = change(node2);
+
+console.log(isBalance(newRoot));
